9db27e1e3bbf63cea3b1839b4ac25f4799e67f4b
LucianoLosada23 <145129031+LucianoLosada23@users.noreply.github.com>
2025-05-24 17:12:44 -0300

    rama admin

--- src/router.tsx
+++ src/router.tsx
@@ -6,32 +6,43 @@ import Order from "./views/Order";
 import UserAddresses from "./components/user/UserAddresses";
 import UserProfile from "./components/user/UserProfile";
 import UserOrders from "./components/user/UserOrders";
+import AdminLayaout from "./components/layout/AdminLayaout";
+
 export const router = createBrowserRouter([
+  // Layout general para usuarios
   {
     path: "/",
     element: <Layout />,
-
     children: [
       {
-        element: <Landing />,
         index: true,
+        element: <Landing />,
       },
       {
         path: "catalogo",
         element: <Order />,
       },
       {
-        path: "/usuario/direcciones",
-        element : <UserAddresses/>
+        path: "usuario/direcciones",
+        element: <UserAddresses />,
       },
       {
-        path: "/usuario/perfil",
-        element : <UserProfile/>
+        path: "usuario/perfil",
+        element: <UserProfile />,
       },
       {
-        path: "/usuario/ordenes",
-        element : <UserOrders/>
+        path: "usuario/ordenes",
+        element: <UserOrders />,
       },
+    ],
+  },
+
+  // Layout separado para admin (sin el layout general)
+  {
+    path: "/admin",
+    element: <AdminLayaout />,
+    children: [
+      
     ],
   },
 ]);
