567df2cadf853959a6c3d26299d2fccde8fc92fb
GabrielKitanovich <gabriel.kitanovich@gmail.com>
2025-06-30 00:33:33 -0300

    Order creo que ya funciona

--- src/services/admin/order/order.ts
+++ src/services/admin/order/order.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { safeParse } from "valibot";
-import { orderByCompanyListSchema } from "../../../types/shop/order/Order";
+import { orderByCompanyListSchema, orderSchema} from "../../../types/shop/order/Order";
+import { listaDeOrdenesSchema } from "../../../types/user/UserOrder";
 
 export const getAllOrdersByCompany = async () => {
   try {
@@ -11,6 +12,22 @@ export const getAllOrdersByCompany = async () => {
       console.log(data);
 
     const result = safeParse(orderByCompanyListSchema, data);
+    if (result.success) {
+      return result.output;
+    }
+  } catch (error) {
+    console.error("Error al obtener los pedidos:", error);
+  }
+};
+export const getAllOrdersByClient = async () => {
+  try {
+    const url = "http://localhost:8080/api/v1/order/byclient";
+    const { data } = await axios.get(url, {
+      withCredentials: true
+    });
+      console.log(data);
+
+    const result = safeParse(listaDeOrdenesSchema, data);
     if (result.success) {
       return result.output;
     }
--- src/services/user/UserOrder.ts
+++ src/services/user/UserOrder.ts
@@ -4,7 +4,7 @@ import { listaDeOrdenesSchema } from "../../types/user/UserOrder";
 
 export const getAllUserOrder = async () => {
   try {
-    const url = "/public/json/order.json";
+    const url = "http://localhost:8080/api/v1/order/byclient";
     const { data } = await axios.get(url);
     const result = safeParse(listaDeOrdenesSchema, data);
     if (result.success) {
