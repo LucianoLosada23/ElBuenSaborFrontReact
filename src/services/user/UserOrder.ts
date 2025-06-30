632795e429a02b55e1ccec552e9a2f75ddd1f7d3
GabrielKitanovich <gabriel.kitanovich@gmail.com>
2025-06-30 11:09:39 -0300

    Order ahora anda

--- src/services/user/UserOrder.ts
+++ src/services/user/UserOrder.ts
@@ -5,7 +5,8 @@ import { listaDeOrdenesSchema } from "../../types/user/UserOrder";
 export const getAllUserOrder = async () => {
   try {
     const url = "http://localhost:8080/api/v1/order/byclient";
-    const { data } = await axios.get(url);
+    const { data } = await axios.get(url, { withCredentials: true });
+    console.log(data);
     const result = safeParse(listaDeOrdenesSchema, data);
     if (result.success) {
       return result.output;
